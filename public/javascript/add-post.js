3c12041e4721e32495329293e29f4f71e63e5f13
stuart-rickard <stuart@bau-dev.com>
2022-02-13 20:01:20 -0800

    updated post handling

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,8 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_text = document.querySelector('input[name="post-text"]').value;
+  // const post_text = "temporary text";
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
